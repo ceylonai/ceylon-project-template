0d3d8f8f1e9456e1439ee02d980cfd0dd7c41b35
dewmal <dewmal@syigen.com>
2024-08-20 14:22:05 +0530

    Send Message to Human

--- agents/process_agent.py
+++ agents/process_agent.py
@@ -1,7 +1,7 @@
 from ceylon import Agent, on_message
 from loguru import logger
 
-from data.message import Message
+from data.message import Message, SystemMessage, SystemMessageType
 from data.user_details import UserDetails
 
 
@@ -14,6 +14,11 @@ class ProcessAgent(Agent):
     @on_message(Message)
     async def on_chat_message(self, message: Message, agent_id: "str", time: "int"):
         logger.info(f"Received message {message} from {agent_id} at {time}")
+        system_message = SystemMessage(
+            type=SystemMessageType.human_interaction,
+            message=f"What is this?"
+        )
+        await self.broadcast_data(system_message)
 
     async def run(self, inputs: "bytes"):
         logger.info(f"Process agent running with inputs: {inputs}")
--- data/message.py
+++ data/message.py
@@ -1,6 +1,8 @@
+import enum
 from datetime import datetime
+from uuid import uuid4
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_serializer
 
 
 class Message(BaseModel):
@@ -8,3 +10,28 @@ class Message(BaseModel):
     sender: str = Field(min_length=1, max_length=100)
     time: datetime = Field(default_factory=datetime.now)
     type: str = Field(min_length=1, max_length=100)
+
+
+class SystemMessageType(str, enum.Enum):
+    human_interaction = "human_interaction"
+    system_message = "system_message"
+
+
+class SystemMessage(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid4()))
+    type: SystemMessageType = Field(min_length=1, max_length=100, default=SystemMessageType.system_message)
+    message: str = Field(min_length=1, max_length=100)
+    time: datetime = Field(default_factory=datetime.now)
+
+    @field_serializer('time')
+    def serialize_dt(self, dt: datetime, _info):
+        return dt.timestamp()
+
+
+if __name__ == '__main__':
+    msg = SystemMessage(
+        type=SystemMessageType.human_interaction,
+        message="What is this?"
+    )
+    print(msg)
+    print(msg.model_dump())
